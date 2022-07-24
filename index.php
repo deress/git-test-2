1b98f5e54dfb7825a7f2d33ad84f0ee379cafec5
Idris Muhammad <idrismuhammad257@gmail.com>
2022-07-24 23:46:49 +0700

    menambahkan list pada index.php

--- index.php
+++ index.php
@@ -10,4 +10,8 @@
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur sequi voluptate obcaecati. Soluta voluptatem commodi in dolores quaerat non labore eveniet cum! Laboriosam corrupti fugit voluptate repellendus doloremque voluptatem itaque!</p>
 </body>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </html>
