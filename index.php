20b82066f9fc5aa7e2fe4f95a729d0e1ef66ea92
Idris Muhammad <idrismuhammad257@gmail.com>
2022-07-24 23:40:41 +0700

    menambahkan paragraf pada index.php

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur sequi voluptate obcaecati. Soluta voluptatem commodi in dolores quaerat non labore eveniet cum! Laboriosam corrupti fugit voluptate repellendus doloremque voluptatem itaque!</p>
 </body>
 </html>
